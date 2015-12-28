d3842a012f469f489f3873bce5ad0dff652da1ba
SunUntouch <sununtouch@freenet.de>
2015-12-28 22:50:44 +0100

    [smart-sunrise] - implemented vibration

--- app/src/main/java/com/zhun/sununtouch/smart_sunrise/AlarmActivity.java
+++ app/src/main/java/com/zhun/sununtouch/smart_sunrise/AlarmActivity.java
@@ -2,6 +2,7 @@ package com.zhun.sununtouch.smart_sunrise;
 
 import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
+import android.content.Context;
 import android.graphics.Color;
 import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraDevice;
@@ -9,6 +10,7 @@ import android.hardware.camera2.CaptureRequest;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.PowerManager;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
@@ -24,9 +26,12 @@ public class AlarmActivity extends AppCompatActivity {
     private CaptureRequest.Builder mBuilder;
     private CameraDevice           mCameraDevice;
 
+    private Vibrator m_Vibrator;
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        setVibrationStop();
     }
 
     private android.os.Handler dateHandler = new android.os.Handler();
@@ -40,29 +45,83 @@ public class AlarmActivity extends AppCompatActivity {
         Bundle intentExtras = getIntent().getExtras();
         int[] time  = intentExtras.getIntArray(AlarmConstants.WAKEUP_TIME); //Time, Minutes, Snooze
         int[] days  = intentExtras.getIntArray(AlarmConstants.WAKEUP_DAYS); //Monday - Sunday
-        int[] music = intentExtras.getIntArray(AlarmConstants.WAKEUP_MUSIC);// Song, StartTime, Volume, FadIn, FadeInTime, Vibration Aktiv, Vibration Strength
-        final int[] light = intentExtras.getIntArray(AlarmConstants.WAKEUP_LIGHT);// UseScreen, ScreenBrightness, ScreenStrartTime, Color1, Color2, FadeColor, UseLed, LedStartTime
+        int[] music = intentExtras.getIntArray(AlarmConstants.WAKEUP_MUSIC);// Song, StartTime, Volume, FadeIn, FadeInTime, Vibration Aktiv, Vibration Strength
+        int[] light = intentExtras.getIntArray(AlarmConstants.WAKEUP_LIGHT);// UseScreen, ScreenBrightness, ScreenStrartTime, Color1, Color2, FadeColor, UseLed, LedStartTime
 
+        //Boolean Values
+        //Screen
+        boolean useScreen      = (light[0] == 1) ? true : false;
+        boolean useLED         = (light[6] == 1) ? true : false;
+        boolean useColorFading = (light[5] == 1) ? true  : false;
+
+        //Music
+        boolean useFadeIn    = (music[3] == 1) ? true : false;
+        boolean useVibration = (music[5] == 1) ? true : false;
 
         //SCREEN BRIGHTNESS/////////////////////////////////////////////////////////////////////////
-        final boolean useScreen = (light[0] == 1) ? true : false;
-        boolean useLED = (light[6] == 1) ? true : false;
-        if(useScreen){
+        doBrightness(useScreen, useLED, light);
 
-            if(light[2] > light[7])
-                setBrightness(light);
+        //SCREEN VIEWS//////////////////////////////////////////////////////////////////////////////
+        doViews();
+
+        //SCREEN COLOR FADING///////////////////////////////////////////////////////////////////////
+        doColorFading(useColorFading, light);
+
+        //LED///////////////////////////////////////////////////////////////////////////////////////
+        doLED(useLED, light);
+
+        //MUSIC FADE IN/////////////////////////////////////////////////////////////////////////////
+
+        //MUSIC VIBRATION///////////////////////////////////////////////////////////////////////////
+        doVibrate(useVibration, useScreen, useLED, music, light);
+    }
+
+    private String getDayName(Calendar _calendar){
+        String dayName = "";
+        switch(_calendar.get(Calendar.DAY_OF_WEEK)){
+            case Calendar.MONDAY:
+                dayName = this.getString(R.string.wakeup_day_monday);
+                break;
+            case Calendar.TUESDAY:
+                dayName = this.getString(R.string.wakeup_day_tuesday);
+                break;
+            case Calendar.WEDNESDAY:
+                dayName = this.getString(R.string.wakeup_day_wednesday);
+                break;
+            case Calendar.THURSDAY:
+                dayName = this.getString(R.string.wakeup_day_thursday);
+                break;
+            case Calendar.FRIDAY:
+                dayName = this.getString(R.string.wakeup_day_friday);
+                break;
+            case Calendar.SATURDAY:
+                dayName = this.getString(R.string.wakeup_day_saturday);
+                break;
+            case Calendar.SUNDAY:
+                dayName = this.getString(R.string.wakeup_day_sunday);
+                break;
+        }
+        return dayName;
+    }
+
+    private void doBrightness(boolean _useScreen, boolean _useLED, final int[] _lightValues){
+
+        if(_useScreen){
+
+            if(_lightValues[2] > _lightValues[7])
+                setBrightness(_lightValues);
             else {
                 android.os.Handler screenLightHandler = new android.os.Handler();
                 Runnable screenLightRunnable = new Runnable() {
                     @Override
                     public void run() {
-                        setBrightness(light);
+                        setBrightness(_lightValues);
                     }
                 };
-                screenLightHandler.postDelayed(screenLightRunnable, TimeUnit.MINUTES.toMillis(light[7] - light[2]));
+                screenLightHandler.postDelayed(screenLightRunnable, TimeUnit.MINUTES.toMillis(_lightValues[7] - _lightValues[2]));
             }
         }
-        else if(useLED){
+        else if(_useLED){
 
             android.os.Handler screenTimerHandler = new android.os.Handler();
             Runnable screenTimerRunnable = new Runnable() {
@@ -71,13 +130,13 @@ public class AlarmActivity extends AppCompatActivity {
                     startScreen();
                 }
             };
-            screenTimerHandler.postDelayed(screenTimerRunnable, TimeUnit.MINUTES.toMillis(light[7]));
+            screenTimerHandler.postDelayed(screenTimerRunnable, TimeUnit.MINUTES.toMillis(_lightValues[7]));
         }
         else{
             startScreen();
         }
-
-        //SCREEN////////////////////////////////////////////////////////////////////////////////////
+    }
+    private void doViews(){
         //SetViews
         //Date
         Calendar calendar = Calendar.getInstance();
@@ -115,17 +174,15 @@ public class AlarmActivity extends AppCompatActivity {
             }
         };
         timeHandler.postDelayed(timeRunnable, 100); //1 second check for new Time
+    }
+    private void doColorFading(boolean _colorFading, final int[] _lightValues){
 
-
-        //COLOR FADING//////////////////////////////////////////////////////////////////////////////
         final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.wakeup_wakescreen_layout);
 
-
-        boolean colorFading = (light[5] == 1) ? true  : false;
-        if(colorFading){
+        if(_colorFading){
             linearLayout.setBackgroundColor(Color.BLACK);
-            ObjectAnimator colorFade1 = ObjectAnimator.ofObject(linearLayout,"backgroundColor", new ArgbEvaluator(), Color.BLACK, light[3]);
-            final long duration = TimeUnit.MINUTES.toMillis(light[2]) / 4;
+            ObjectAnimator colorFade1 = ObjectAnimator.ofObject(linearLayout,"backgroundColor", new ArgbEvaluator(), Color.BLACK, _lightValues[3]);
+            final long duration = TimeUnit.MINUTES.toMillis(_lightValues[2]) / 4;
             colorFade1.setDuration(duration);
             colorFade1.start();
 
@@ -134,7 +191,7 @@ public class AlarmActivity extends AppCompatActivity {
                 @Override
                 public void run() {
 
-                    ObjectAnimator colorFade2 = ObjectAnimator.ofObject(linearLayout,"backgroundColor", new ArgbEvaluator(), light[3], light[4]);
+                    ObjectAnimator colorFade2 = ObjectAnimator.ofObject(linearLayout,"backgroundColor", new ArgbEvaluator(), _lightValues[3], _lightValues[4]);
                     colorFade2.setDuration(duration * 3);
                     colorFade2.start();
 
@@ -143,12 +200,12 @@ public class AlarmActivity extends AppCompatActivity {
             colorHandler.postDelayed(colorRunnable, duration );
         }
         else{
-            linearLayout.setBackgroundColor(light[3]);
+            linearLayout.setBackgroundColor(_lightValues[3]);
         }
-
-        //LED///////////////////////////////////////////////////////////////////////////////////////
-        if(useLED){
-            if(light[2] > light[7]) { //If LED StartTime is bigger as LED Start Time
+    }
+    private void doLED(boolean _useLED, final int[] _lightValues){
+        if(_useLED){
+            if(_lightValues[2] > _lightValues[7]) { //If LED StartTime is bigger as LED Start Time
 
                 //New Handler for Waiting Till Time to show LED
                 final android.os.Handler handler = new android.os.Handler();
@@ -161,38 +218,42 @@ public class AlarmActivity extends AppCompatActivity {
                 };
 
                 //Start Delayed
-                handler.postDelayed(runLED, TimeUnit.MINUTES.toMillis(light[2] - light[7]));
+                handler.postDelayed(runLED, TimeUnit.MINUTES.toMillis(_lightValues[2] - _lightValues[7]));
             } else
                 startLED();
         }
     }
 
-    private String getDayName(Calendar _calendar){
-        String dayName = "";
-        switch(_calendar.get(Calendar.DAY_OF_WEEK)){
-            case Calendar.MONDAY:
-                dayName = this.getString(R.string.wakeup_day_monday);
-                break;
-            case Calendar.TUESDAY:
-                dayName = this.getString(R.string.wakeup_day_tuesday);
-                break;
-            case Calendar.WEDNESDAY:
-                dayName = this.getString(R.string.wakeup_day_wednesday);
-                break;
-            case Calendar.THURSDAY:
-                dayName = this.getString(R.string.wakeup_day_thursday);
-                break;
-            case Calendar.FRIDAY:
-                dayName = this.getString(R.string.wakeup_day_friday);
-                break;
-            case Calendar.SATURDAY:
-                dayName = this.getString(R.string.wakeup_day_saturday);
-                break;
-            case Calendar.SUNDAY:
-                dayName = this.getString(R.string.wakeup_day_sunday);
-                break;
+    private void doFadeInMusic(){
+    }
+    private void doVibrate(boolean _vibrate, boolean _useScreen, boolean _useLED, final int[] _musicValues, int[] _lightValues){
+
+        if(_useLED || _useScreen){
+
+            if(_vibrate){
+                Handler vibrationHandler = new Handler();
+                Runnable vibrationRunnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        setVibrationStart(_musicValues[6], 100);
+                    }
+                };
+
+                long millis = 0;
+                if(_useLED && _useScreen)
+                    millis = (_lightValues[2] > _lightValues[7]) ?
+                            TimeUnit.MINUTES.toMillis(_lightValues[2]) : TimeUnit.MINUTES.toMillis(_lightValues[7]);
+                else if(_useScreen)
+                    millis = TimeUnit.MINUTES.toMillis(_lightValues[2]);
+                else if(_useLED)
+                    millis = TimeUnit.MINUTES.toMillis(_lightValues[7]);
+
+                vibrationHandler.postDelayed(vibrationRunnable, millis);
+            }
         }
-        return dayName;
+        else
+            setVibrationStart(_musicValues[6], 100);
+
     }
 
     private void setBrightness(final int[] _lightValues){
@@ -222,6 +283,47 @@ public class AlarmActivity extends AppCompatActivity {
             }
         };
         screenLightHandler.postDelayed(screenLightRunnable, millis); //All 10 Seconds more Light
+    }
+
+    private void setVibrationStart(){
+        long[] pattern = { 0, 100, 200, 300, 400 };
+        setVibrationStart(pattern, 0);
+    }
+    private void setVibrationStart(int _intensity, long _duration){
+        long[] pattern = generateVibratorpatter(_intensity, _duration);
+        setVibrationStart(pattern, 0);
+    }
+    private void setVibrationStart(long[] _pattern, int _repeat){
+        m_Vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        m_Vibrator.vibrate(_pattern, _repeat);
+    }
+
+    private long[] generateVibratorpatter(int _intensity, long duration){
+
+        float intensity =  ((float) _intensity) / 100.0f;
+
+        float dutyCycle = Math.abs( (intensity * 2.0f) - 1.0f );
+
+        long hWidth = (long) (dutyCycle * ( duration - 1 )) + 1;
+        long lWidth = (dutyCycle == 1.0f)? 0 : 1;
+
+        int pulseCount = (int) (2.0f * ((float) duration / (float) (hWidth + lWidth)));
+        long[] pattern = new long[pulseCount];
+
+        for(int i = 0; i < pulseCount; ++i){
+            pattern[i] = (intensity < 0.5f) ?
+                    ((i % 2 == 0) ? hWidth : lWidth) : ( (i % 2 == 0) ? lWidth : hWidth );
+        }
+
+        return pattern;
+
+    }
+
+    private void setVibrationStop(){
+
+        if(m_Vibrator != null){
+            m_Vibrator.cancel();
+        }
     }
 
     private void startScreen(){
