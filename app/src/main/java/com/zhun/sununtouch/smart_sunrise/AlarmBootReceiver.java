d40c4ce302553fce49e23ac1e154635db703aed6
sununtouch <sununtouch@freenet.de>
2017-01-02 18:12:12 +0100

    [smart sunrise] - implemented on Boot reactivation if needed

---
+++ app/src/main/java/com/zhun/sununtouch/smart_sunrise/AlarmBootReceiver.java
@@ -0,0 +1,24 @@
+package com.zhun.sununtouch.smart_sunrise;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Sunny on 02.01.2017.
+ */
+
+public class AlarmBootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if(intent != null && intent.getAction() != null && "android.intent.action.BOOT_COMPLETED".equals(intent.getAction()))
+        {
+            //This is the ON BOOT Intent
+            AlarmConfiguration alarm = new AlarmConfiguration(context, intent.getExtras().getInt(AlarmConstants.ALARM_ID));
+            if(alarm.isDaySet() || alarm.getAlarmOneShot())
+                alarm.activateAlarm();
+        }
+    }
+}
--- app/src/main/java/com/zhun/sununtouch/smart_sunrise/AlarmIntentService.java
+++ app/src/main/java/com/zhun/sununtouch/smart_sunrise/AlarmIntentService.java
@@ -10,22 +10,11 @@ public class AlarmIntentService extends IntentService {
     }
     @Override
     protected void onHandleIntent(Intent intent) {
-
-        if("android.intent.action.BOOT_COMPLETED".equals(intent.getAction()))
-        {
-            //This is the ON BOOT Intent
-            AlarmConfiguration alarm = new AlarmConfiguration(getApplicationContext(), intent.getExtras().getInt(AlarmConstants.ALARM_ID));
-            if(alarm.isDaySet() || alarm.getAlarmOneShot())
-                alarm.activateAlarm();
-        }
-        else if(intent.getAction() == null)
-        {
-            //this is the alarm Intent
-            Intent alarmIntent = new Intent(this, AlarmActivity.class);
-            alarmIntent.putExtras(intent);
-            alarmIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(alarmIntent);
-            AlarmReceiver.completeWakefulIntent(intent);
-        }
+        //this is the alarm Intent
+        Intent alarmIntent = new Intent(this, AlarmActivity.class);
+        alarmIntent.putExtras(intent);
+        alarmIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(alarmIntent);
+        AlarmReceiver.completeWakefulIntent(intent);
     }
 }
